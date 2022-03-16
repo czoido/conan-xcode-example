701efe29915610f269c6c5af929654f648eeb697
czoido <mrgalleta@gmail.com>
2022-03-16 15:35:16 +0100

    wip

--- conanfile.py
+++ conanfile.py
@@ -2,6 +2,9 @@ import os
 from conan import ConanFile
 from conan.tools.apple import XcodeBuild
 from conan.tools.files import copy
+from conan.tools.scm import Git
+from conan.tools.files import load, update_conandata
+
 
 class HelloLib(ConanFile):
     name = "hello"
@@ -9,6 +12,12 @@ class HelloLib(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "XcodeToolchain"
     exports_sources = "HelloLibrary.xcodeproj/*", "src/*"
+
+    def export(self):
+        git = Git(self, self.recipe_folder)
+        scm_url, scm_commit = git.get_url_and_commit()
+        # we store the current url and commit in conandata.yml
+        update_conandata(self, {"sources": {"commit": scm_commit, "url": scm_url}})
 
     def build(self):
         xcode = XcodeBuild(self)
